844b7a647b48d176aa341489b8723d7d84d24c8a
Opeyemi Alabi <91141140+horluwaluvme@users.noreply.github.com>
2023-02-08 14:42:53 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,27 +3,25 @@
 '''
 
 
-
 def minOperations(n):
-
 
     """
     Compute the fewest number of operations needed to result 
     in exactly n H characters in the file.
-    """
+    """ 
     
     normal = 0
-
+    
     minumum = 2
-
+    
     while n > 1:
-
+    
         while n % minumum == 0:
-
+        
             normal += minumum
-
+            
             n /= minumum
-
+        
         minumum += 1
-
+    
     return normal
