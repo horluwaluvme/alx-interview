73683b8450376adcb8a42d4ee1d8a488c6bc70ac
Opeyemi Alabi <91141140+horluwaluvme@users.noreply.github.com>
2023-02-17 20:09:27 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,15 @@
-
 #!/usr/bin/python3
-"""Script that reads stdin line by line and computes metrics"""
+import random
 import sys
+from time import sleep
+import datetime
 
-
-def print_metrics(status_codes, file_size):
-    """function to print status codes and total file size"""
-    print("File size: {}".format(file_size))
-    for k, v in sorted(status_codes.items()):
-        if v > 0:
-            print("{}: {}".format(k, v))
-
-
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
-}
-counter = 1
-file_size = 0
-try:
-    for line in sys.stdin:
-        splited_line = line.strip().split()
-        if len(splited_line) < 7:
-            continue
-        file_size += int(splited_line[-1])
-        status_code = splited_line[-2]
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-        if counter == 10:
-            print_metrics(status_codes, file_size)
-            counter = 1
-        else:
-            counter += 1
-    print_metrics(status_codes, file_size)
-except KeyboardInterrupt:
-    print_metrics(status_codes, file_size)
-    raise
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
