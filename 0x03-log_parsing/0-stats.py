e16f4feeea5e0a9833f8d89fb70892f96daaae80
Opeyemi Alabi <91141140+horluwaluvme@users.noreply.github.com>
2023-02-19 06:01:13 +0100

    Commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,91 +1,86 @@
 #!/usr/bin/python3
-"""Example Google style docstrings.
-This module demonstrates documentation as specified by the `Google Python
-Style Guide`_. Docstrings may extend over multiple lines. Sections are created
-with a section header and a colon followed by a block of indented text.
-Example:
-    Examples can be given using either the ``Example`` or ``Examples``
-    sections. Sections support any reStructuredText formatting, including
-    literal blocks::
-        $ python example_google.py
-Section breaks are created by resuming unindented text. Section breaks
-are also implicitly created anytime a new section starts.
-Attributes:
-    module_level_variable1 (int): Module level variables may be documented in
-        either the ``Attributes`` section of the module docstring, or in an
-        inline docstring immediately following the variable.
-        Either form is acceptable, but the two should not be mixed. Choose
-        one convention to document module level variables and be consistent
-        with it.
-Todo:
-    * For module TODOs
-    * You have to also use ``sphinx.ext.todo`` extension
-.. _Google Python Style Guide:
-   http://google.github.io/styleguide/pyguide.html
-"""
-
-import sys
+'''A script for parsing HTTP request logs.
+'''
 import re
 
 
-def validate_user_input(user_input: str) -> bool:
-    """
-    Validate the user_input to conform with the format
-    """
-    ip = r'^(\d{1,255}\.){3,3}\d{1,255} '
-    mid = r'- \[.+\] "GET \/projects\/260 HTTP\/1.1" '
-    code = r'(200|301|400|401|403|404|405|500) \d+$'
-    pat = ip + mid + code
-    pattern = re.compile(pat)
-    return re.match(pattern, user_input)
+def extract_input(input_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
 
-def get_sc_and_fs(user_input: str):
-    """
-    Get the status code and the file size from the validated user_input
-    """
-    splited = re.split(r'(\d+ \d+$)', user_input)
-    status_code, file_size = re.split(' ', splited[1])
-    return status_code, file_size
+def print_statistics(total_file_size, status_codes_stats):
+    '''Prints the accumulated statistics of the HTTP request log.
+    '''
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
 
-def print_all(file_size, status_codes):
-    """
-    Print the current total file size and the counts of status codes
-    """
-    print("File size: {}".format(file_size))
-    for status_code in sorted(status_codes):
-        if not status_code.isnumeric():
-            pass
-        print("{}: {}".format(status_code, status_codes.get(status_code)))
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates the metrics from a given HTTP request log.
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+    Returns:
+        int: The new total file size.
+    '''
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
 
 
-def log_parser():
-    """
-    The main function. Continue to loop until the Keyboard Interrupt is
-    pressed
-    """
+def run():
+    '''Starts the log parser.
+    '''
+    line_num = 0
     total_file_size = 0
-    status_codes_count = {}
-    loop_count = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
     try:
-        while (True):
-            user_input = sys.stdin.readline()
-            loop_count += 1
-            if validate_user_input(user_input) is None:
-                user_input = sys.stdin.readline()
-                continue
-            status_code, file_size = get_sc_and_fs(user_input)
-            total_file_size += int(file_size)
-            if status_codes_count.get(status_code) is None:
-                status_codes_count[status_code] = 0
-            status_codes_count[status_code] += 1
-            if loop_count == 10:
-                print_all(total_file_size, status_codes_count)
-                loop_count = 0
-    except KeyboardInterrupt:
-        print_all(total_file_size, status_codes_count)
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
 
 
-if "__main__" == __name__:
-    log_parser()
+if __name__ == '__main__':
+    run()
