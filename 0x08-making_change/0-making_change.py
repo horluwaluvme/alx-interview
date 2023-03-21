4afca7cac520ae41494316e6906c06b81f991d02
root <pymfemi@gmail.com>
2023-03-21 15:45:49 -0700

    making changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
+"""Make Change.
 """
-defining the function
-"""
+
 
 def makeChange(coins, total):
-    # initialize a table to store the minimum number of coins needed for each sub-total
-    table = [float('inf')] * (total + 1)
-    table[0] = 0
-    
-    # iterate over each coin
-    for coin in coins:
-        # for each coin, iterate over each sub-total
-        for i in range(coin, total+1):
-            # calculate the minimum number of coins needed for the current sub-total
-            table[i] = min(table[i], table[i-coin]+1)
-    
-    # if the final sub-total is still infinity, it means the total cannot be met
-    if table[total] == float('inf'):
-        return -1
-    
-    # otherwise, return the minimum number of coins needed to meet the total
-    return table[total]
-
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
